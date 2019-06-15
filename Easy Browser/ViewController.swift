b15f7fae83f880725a5afc5762e9464acfea569f
Sanjeev Bedasee <ibedasee@gmail.com>
2019-06-15 16:35:55 -0400

    Initial Commit

---
+++ Easy Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Mac on 6/15/19.
+//  Copyright © 2019 Sbedx4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
